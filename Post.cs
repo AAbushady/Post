86f4eae65853ede8d70a91dac665b40a7dd061f4
Alex Abushady <aabushady214@gmail.com>
2019-12-10 14:52:57 -0500

    Add project files.

---
+++ Post.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reflection;
+
+namespace Post
+{
+    public class Post
+    {
+        private readonly string _title;
+        private readonly string _description;
+        private readonly DateTime _created = DateTime.Now;
+        private int _votes;
+        private string _template;
+
+        public Post(string title, string description)
+        {
+            _title = title;
+            _description = description;
+            _votes = 0;
+        }
+
+        public void UpVote()
+        {
+            this._votes++;
+        }
+
+        public void DownVote()
+        {
+            _votes--;
+        }
+
+        public string ReadPost()
+        {
+
+            _template = $"\n{_title}\n{_description}\nThis post has {_votes} votes.";
+            return _template;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Post
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var post = PostCreator();
+
+            Console.WriteLine("Here is your first post.");
+
+            PostDisplay(post);
+            
+            var input = Console.ReadLine();
+
+            while (input.ToLower() != "quit")
+            {
+                switch (input.ToLower())
+                {
+                    case "up":
+                        post.UpVote();
+                        break;
+                    case "down":
+                        post.DownVote();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Input please try again...");
+                        break;
+                }
+                Console.Clear();
+
+                PostDisplay(post);
+                
+                input = Console.ReadLine();
+            }
+        }
+
+        public static Post PostCreator()
+        {
+            Console.WriteLine("Please create a post.");
+
+            Console.Write("Title: ");
+            var title = Console.ReadLine();
+            Console.Write("Description: ");
+            var description = Console.ReadLine();
+
+            return new Post(title, description);
+        }
+
+        public static void PostDisplay(Post post)
+        {
+            Console.WriteLine(post.ReadPost());
+            Console.Write("\nType \"Up\" to up vote, \"Down\" to down vote, or \"Quit\" to leave: ");
+        }
+    }
+}
